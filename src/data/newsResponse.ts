96eb1a3e5af96c5083682fc4e35765a7145897ff
basuabhirup <basu.abhirup@yahoo.com>
2022-03-23 17:26:10 +0530

    Set up project & add sample dataset

---
+++ src/data/newsResponse.ts
@@ -0,0 +1,136 @@
+export const newsResponse = {
+  "status": "ok",
+  "totalResults": 10,
+  "articles": [
+    {
+      "source": {
+        "id": "techcrunch",
+        "name": "TechCrunch"
+      },
+      "author": "Kate Park",
+      "title": "Korean startup Dongnae secures $21M Series A to scale apartment rental service",
+      "description": "Dongnae, a South Korea-based startup that helps tenants find premium apartment rentals, has closed $16.4 million (20 billion KRW) in equity and $4.1 million (5 billion KRW) in debt. The Series A funding brings its total funding raised to approximately $34 mil…",
+      "url": "https://techcrunch.com/2022/03/23/korean-startup-dongnae-secures-21m-series-a-to-scale-apartment-rental-service/",
+      "urlToImage": "https://techcrunch.com/wp-content/uploads/2022/03/Dongnae-leadership-team.jpeg?w=533",
+      "publishedAt": "2022-03-23T09:30:10Z",
+      "content": "Dongnae, a South Korea-based startup that helps tenants find premium apartment rentals, has closed $16.4 million (20 billion KRW) in equity and $4.1 million (5 billion KRW) in debt. The Series A fund… [+4416 chars]"
+    },
+    {
+      "source": {
+        "id": "techcrunch",
+        "name": "TechCrunch"
+      },
+      "author": "Sarah Perez",
+      "title": "YouTube to stream free, ad-supported TV shows for the first time",
+      "description": "YouTube today announced it will for the first time stream free, ad-supported TV shows — a move that puts in into more direct competition with the growing number of free streaming services on the market, including Tubi, Pluto TV, The Roku Channel, IMDb TV, Xum…",
+      "url": "https://techcrunch.com/2022/03/23/youtube-to-stream-free-ad-supported-tv-shows-for-the-first-time/",
+      "urlToImage": "https://techcrunch.com/wp-content/uploads/2022/03/Storefront-AVOD-shelves.png?w=671",
+      "publishedAt": "2022-03-23T07:00:46Z",
+      "content": "YouTube today announced it will for the first time stream free, ad-supported TV shows — a move that puts in into more direct competition with the growing number of free streaming services on the mark… [+3791 chars]"
+    },
+    {
+      "source": {
+        "id": "techcrunch",
+        "name": "TechCrunch"
+      },
+      "author": "Kate Park",
+      "title": "Japan's Terra Drone lands $70M Series B funding to accelerate global expansion",
+      "description": "As the use of aircraft like drones, electric vertical take-off and landing vehicles and uncrewed aerial vehicles take off, Japanese startup Terra Drone wants to make sure trafficking the skies doesn’t get overlooked. “Our airspace is going to get more crowded…",
+      "url": "https://techcrunch.com/2022/03/22/japans-terra-drone-lands-70m-series-b-funding-to-accelerate-global-expansion/",
+      "urlToImage": "https://techcrunch.com/wp-content/uploads/2022/03/UTM1.png?w=564",
+      "publishedAt": "2022-03-23T00:20:59Z",
+      "content": "As the use of aircraft like drones, electric vertical take-off and landing vehicles and uncrewed aerial vehicles take off, Japanese startup Terra Drone wants to make sure trafficking the skies doesn’… [+4602 chars]"
+    },
+    {
+      "source": {
+        "id": "techcrunch",
+        "name": "TechCrunch"
+      },
+      "author": "Alex Wilhelm, Christine Hall",
+      "title": "Daily Crunch: Identity-as-a-service platform Okta says it 'contained' network breach in January",
+      "description": "Hello friends and welcome to Daily Crunch, bringing you the most important startup, tech and venture capital news in a single package.",
+      "url": "https://techcrunch.com/2022/03/22/daily-crunch-identity-as-a-service-platform-okta-says-it-contained-network-breach-in-january/",
+      "urlToImage": "https://techcrunch.com/wp-content/uploads/2022/03/GettyImages-1231471805.jpg?w=599",
+      "publishedAt": "2022-03-22T22:30:40Z",
+      "content": "To get a roundup of TechCrunchs biggest and most important stories delivered to your inbox every day at 3 p.m. PT, subscribe here.\r\nHello and welcome to Daily Crunch for Tuesday, March 22, 2022! Exci… [+8189 chars]"
+    },
+    {
+      "source": {
+        "id": "techcrunch",
+        "name": "TechCrunch"
+      },
+      "author": "Rebecca Bellan",
+      "title": "Former Cruise CEO Dan Ammann joins ExxonMobil",
+      "description": "Former Cruise CEO Dan Ammann moved has to ExxonMobil. He'll be focused on opportunities in carbon capture/storage, hydrogen and low-emission fuels.",
+      "url": "https://techcrunch.com/2022/03/22/former-cruise-ceo-dan-ammann-joins-exxonmobil/",
+      "urlToImage": "https://techcrunch.com/wp-content/uploads/2022/03/AP21350802490403.jpg?w=711",
+      "publishedAt": "2022-03-22T21:58:44Z",
+      "content": "Dan Ammann, the New Zealand native who served as CEO of autonomous vehicle company Cruise since 2019, has moved on to ExxonMobil, where he will be leading a new business focused on commercializing lo… [+1679 chars]"
+    },
+    {
+      "source": {
+        "id": "techcrunch",
+        "name": "TechCrunch"
+      },
+      "author": "Connie Loizos",
+      "title": "Alphabet spins out a new subsidiary, and in a sign of the times, it's focused on quantum tech",
+      "description": "Quantum tech may be having its moment at long last. Early this month, one of the few “pure play” quantum tech companies in the world, Rigetti Computing, went public by merging with a special purpose acquisition company or SPAC — and it only narrowly missed be…",
+      "url": "https://techcrunch.com/2022/03/22/alphabet-spins-out-a-new-subsidiary-and-in-a-sign-of-the-times-its-focused-on-quantum-tech/",
+      "urlToImage": "https://techcrunch.com/wp-content/uploads/2020/09/GettyImages-1147600063.jpg?w=587",
+      "publishedAt": "2022-03-22T21:26:04Z",
+      "content": "Quantum tech may be having its moment at long last.\r\nEarly this month, one of the few “pure play” quantum tech companies in the world, Rigetti Computing, went public by merging with a special purpose… [+3909 chars]"
+    },
+    {
+      "source": {
+        "id": "techcrunch",
+        "name": "TechCrunch"
+      },
+      "author": "Vijay Chattha, Jay Kapoor",
+      "title": "It's time to hold investors accountable and abolish pro-rata",
+      "description": "We're now in the era of \"value-add venture capital,\" where investors need to show founders that they will do more for them than merely cut a check.",
+      "url": "https://techcrunch.com/2022/03/22/its-time-to-hold-investors-accountable-and-abolish-pro-rata/",
+      "urlToImage": "https://techcrunch.com/wp-content/uploads/2022/03/GettyImages-1284113343.jpg?w=562",
+      "publishedAt": "2022-03-22T21:23:53Z",
+      "content": "More posts by this contributor\r\nVC has changed a lot since I was pounding the pavement on Sand Hill Road as a young entrepreneur in the late ’90s.\r\nCapital was hard to come by, and founders had to pr… [+2842 chars]"
+    },
+    {
+      "source": {
+        "id": "techcrunch",
+        "name": "TechCrunch"
+      },
+      "author": "Manish Singh",
+      "title": "Web3 digital identity startup Unstoppable Domains said to seek funding at $1 billion valuation",
+      "description": "Unstoppable Domains, a popular blockchain naming system provider, is putting together a funding round at a valuation of $1 billion, three people familiar with the matter told TechCrunch. The startup is in talks with a number of new and existing investors incl…",
+      "url": "https://techcrunch.com/2022/03/22/web3-digital-identity-startup-unstoppable-domains-said-to-seek-funding-at-1-billion-valuation/",
+      "urlToImage": "https://techcrunch.com/wp-content/uploads/2022/03/unstoppable-domains-1.jpg?w=711",
+      "publishedAt": "2022-03-22T20:39:47Z",
+      "content": "Unstoppable Domains, a popular blockchain naming system provider, is putting together a funding round at a valuation of $1 billion, three people familiar with the matter told TechCrunch.\r\nThe startup… [+1926 chars]"
+    },
+    {
+      "source": {
+        "id": "techcrunch",
+        "name": "TechCrunch"
+      },
+      "author": "Amanda Silberling",
+      "title": "You can now make GIFs in Twitter's iOS app",
+      "description": "When you compose a new tweet on iOS, click the camera button. Then, you have the option to choose between a photo, a video, and an animated GIF.",
+      "url": "https://techcrunch.com/2022/03/22/twitter-gifs-ios-app-feature/",
+      "urlToImage": "https://techcrunch.com/wp-content/uploads/2021/10/twitter-2021-10-b.jpg?w=711",
+      "publishedAt": "2022-03-22T19:51:07Z",
+      "content": "If you ever wanted to make a GIF from your iOS camera while using the Twitter app, today’s your lucky day. Twitter announced that now, we can do just that.\r\nWhen you compose a new tweet on iOS, click… [+1097 chars]"
+    },
+    {
+      "source": {
+        "id": "techcrunch",
+        "name": "TechCrunch"
+      },
+      "author": "Lucas Matney",
+      "title": "'Bored Apes' NFT startup Yuga Labs raises at monster $4 billion valuation",
+      "description": "Yuga Labs, maker of the multimillion monkey NFTs that plenty of people love to hate, just raised a $450 million round from Andreessen Horowitz at a $4 billion valuation, the company announced Tuesday. The NFT firm behind Bored Apes Yacht Club has not previous…",
+      "url": "https://techcrunch.com/2022/03/22/bored-apes-nft-startup-yuga-labs-raises-at-monster-4-billion-valuation/",
+      "urlToImage": "https://techcrunch.com/wp-content/uploads/2022/03/nWay-BAYC-blockchain-game-1-e1647461653135.png?w=764",
+      "publishedAt": "2022-03-22T19:18:10Z",
+      "content": "Yuga Labs, maker of the multimillion monkey NFTs that plenty of people love to hate, just raised a $450 million round from Andreessen Horowitz at a $4 billion valuation, the company announced Tuesday… [+588 chars]"
+    }
+  ]
+}
